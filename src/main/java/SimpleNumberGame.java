1b0467b838a4d643e2d881bf6546076c919be09c
sam kim <seungkim11@gmail.com>
2016-06-26 15:03:16 -0700

    homework3 published

---
+++ src/main/java/SimpleNumberGame.java
@@ -0,0 +1,21 @@
+/**
+ * Created by samskim on 6/26/16.
+ */
+public class SimpleNumberGame {
+
+
+    public static void main(String[] args){
+
+        // create a game of simple number game of 2 players
+        // Game rules:
+        // 1. Each player takes turn to choose a number from 1-11
+        // 2. Whoever that has the higher sum wins
+        // 3. If a number is dealt already, that number cannot be dealt again. (hint: use Set<Integer> to keep track
+        // 4. Print Each player's sum of numbers, numbers they were dealt, each turn
+        //    hint: create another Player class, use List<Integer> to store the numbers
+        // 5. The game ends when all of the numbers 1-11 run out
+        // 6. Show which player wins
+        // Next week, we'll go over the answers
+    }
+
+}
